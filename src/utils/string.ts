36350a11a6f0227e03cd788ea1606ead29c313a2
Backend Architect <architect@deliveryapp.com>
2025-12-31 08:29:48 -0500

    feat: add string manipulation utilities

---
+++ src/utils/string.ts
@@ -0,0 +1,54 @@
+/**
+ * String Manipulation Utilities
+ */
+
+/**
+ * Capitalize first letter
+ */
+export function capitalize(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+/**
+ * Convert to title case
+ */
+export function toTitleCase(str: string): string {
+  return str
+    .split(' ')
+    .map(word => capitalize(word))
+    .join(' ');
+}
+
+/**
+ * Truncate string
+ */
+export function truncate(str: string, length: number, suffix: string = '...'): string {
+  if (str.length <= length) return str;
+  return str.substring(0, length - suffix.length) + suffix;
+}
+
+/**
+ * Generate slug from string
+ */
+export function slugify(str: string): string {
+  return str
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '');
+}
+
+/**
+ * Remove HTML tags
+ */
+export function stripHtml(html: string): string {
+  return html.replace(/<[^>]*>/g, '');
+}
+
+/**
+ * Escape special regex characters
+ */
+export function escapeRegex(str: string): string {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
